7cab378d8027c754156eea5b8443f22ae01410e4
James Bonaiuto <jbonaiuto@gmail.com>
2012-04-28 18:35:37 -0700

    added ezrscluter support

--- src/python/pysbi/wta_jobs.py
+++ src/python/pysbi/wta_jobs.py
@@ -6,7 +6,7 @@ from pysbi.config import SRC_DIR
 def get_wta_commands(num_groups, input_pattern, trial_duration, p_b_e, p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, 
                      record_voxel=False, record_neuron_state=False, record_spikes=False, record_firing_rate=True):
     cmds = ['python', '/tmp/pySBI/src/python/pysbi/wta.py']
-    file_desc='wta.groups.%d.input.%s.duration.%0.3f.p_b_e.%0.3f.p_x_e.%0.3f.p_e_e.%0.3f.p_e_i.%0.3f.p_i_i.%0.3f.p_i_e.%0.3f' %\ 
+    file_desc='wta.groups.%d.input.%s.duration.%0.3f.p_b_e.%0.3f.p_x_e.%0.3f.p_e_e.%0.3f.p_e_i.%0.3f.p_i_i.%0.3f.p_i_e.%0.3f' %\
               (num_groups, input_pattern, trial_duration, p_b_e, p_x_e, p_e_e, p_e_i, p_i_i, p_i_e)
     log_file_template='%s.log' % file_desc
     output_file='/tmp/wta-output/%s.h5' % file_desc
