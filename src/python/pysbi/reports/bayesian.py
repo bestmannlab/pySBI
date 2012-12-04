d3e85368a9a61904efef6a20c9873a053650f3d4
James Bonaiuto <jbonaiuto@gmail.com>
2012-12-03 16:51:44 -0800

    set ylim of joint marginal plots

--- src/python/pysbi/reports/bayesian.py
+++ src/python/pysbi/reports/bayesian.py
@@ -5,7 +5,7 @@ from pysbi.util.utils import Struct, save_to_png, save_to_eps
 
 def create_bayesian_report(title, num_groups, trial_duration, roc_auc, bc_slope, bc_intercept, bc_r_sqr, evidence,
                            posterior, marginals, p_b_e_range, p_x_e_range, p_e_e_range, p_e_i_range, p_i_i_range,
-                           p_i_e_range, file_prefix, reports_dir, edesc):
+                           p_i_e_range, file_prefix, reports_dir, edesc, marginal_ylim):
     report_info=Struct()
     report_info.title=title
     report_info.edesc=edesc
@@ -35,32 +35,32 @@ def create_bayesian_report(title, num_groups, trial_duration, roc_auc, bc_slope,
     report_info.marginal_prior_p_b_e_url,\
     report_info.marginal_likelihood_p_b_e_url,\
     report_info.marginal_posterior_p_b_e_url=render_marginal_report('p_b_e', p_b_e_range,
-        marginals.prior_p_b_e, marginals.likelihood_p_b_e, marginals.posterior_p_b_e, file_prefix, reports_dir)
+        marginals.prior_p_b_e, marginals.likelihood_p_b_e, marginals.posterior_p_b_e, file_prefix, reports_dir, marginal_ylim)
 
     report_info.marginal_prior_p_x_e_url,\
     report_info.marginal_likelihood_p_x_e_url,\
     report_info.marginal_posterior_p_x_e_url=render_marginal_report('p_x_e', p_x_e_range,
-        marginals.prior_p_x_e, marginals.likelihood_p_x_e, marginals.posterior_p_x_e, file_prefix, reports_dir)
+        marginals.prior_p_x_e, marginals.likelihood_p_x_e, marginals.posterior_p_x_e, file_prefix, reports_dir, marginal_ylim)
 
     report_info.marginal_prior_p_e_e_url,\
     report_info.marginal_likelihood_p_e_e_url,\
     report_info.marginal_posterior_p_e_e_url=render_marginal_report('p_e_e', p_e_e_range,
-        marginals.prior_p_e_e, marginals.likelihood_p_e_e, marginals.posterior_p_e_e, file_prefix, reports_dir)
+        marginals.prior_p_e_e, marginals.likelihood_p_e_e, marginals.posterior_p_e_e, file_prefix, reports_dir, marginal_ylim)
 
     report_info.marginal_prior_p_e_i_url,\
     report_info.marginal_likelihood_p_e_i_url,\
     report_info.marginal_posterior_p_e_i_url=render_marginal_report('p_e_i', p_e_i_range,
-        marginals.prior_p_e_i, marginals.likelihood_p_e_i, marginals.posterior_p_e_i, file_prefix, reports_dir)
+        marginals.prior_p_e_i, marginals.likelihood_p_e_i, marginals.posterior_p_e_i, file_prefix, reports_dir, marginal_ylim)
 
     report_info.marginal_prior_p_i_i_url,\
     report_info.marginal_likelihood_p_i_i_url,\
     report_info.marginal_posterior_p_i_i_url=render_marginal_report('p_i_i', p_i_i_range,
-        marginals.prior_p_i_i, marginals.likelihood_p_i_i, marginals.posterior_p_i_i, file_prefix, reports_dir)
+        marginals.prior_p_i_i, marginals.likelihood_p_i_i, marginals.posterior_p_i_i, file_prefix, reports_dir, marginal_ylim)
 
     report_info.marginal_prior_p_i_e_url,\
     report_info.marginal_likelihood_p_i_e_url,\
     report_info.marginal_posterior_p_i_e_url=render_marginal_report('p_i_e', p_i_e_range,
-        marginals.prior_p_i_e, marginals.likelihood_p_i_e, marginals.posterior_p_i_e, file_prefix, reports_dir)
+        marginals.prior_p_i_e, marginals.likelihood_p_i_e, marginals.posterior_p_i_e, file_prefix, reports_dir, marginal_ylim)
 
 
     report_info.joint_marginal_p_b_e_p_x_e_url = render_joint_marginal_report('p_b_e', 'p_x_e', p_b_e_range, p_x_e_range,
@@ -87,7 +87,7 @@ def create_bayesian_report(title, num_groups, trial_duration, roc_auc, bc_slope,
     return report_info
 
 
-def render_marginal_report(param_name, param_range, param_prior, param_likelihood, param_posterior, file_prefix, reports_dir):
+def render_marginal_report(param_name, param_range, param_prior, param_likelihood, param_posterior, file_prefix, reports_dir, ylim):
     if len(param_range) > 1:
         param_step=param_range[1]-param_range[0]
 
@@ -96,7 +96,7 @@ def render_marginal_report(param_name, param_range, param_prior, param_likelihoo
         plt.bar(np.array(param_range) - .5*param_step, param_prior, param_step)
         plt.xlabel(param_name)
         plt.ylabel('p(%s|M)' % param_name)
-        plt.ylim([0,.8])
+        plt.ylim([0,ylim])
         prior_furl = 'img/bayes_%s_marginal_prior_%s.png' % (file_prefix,param_name)
         fname = os.path.join(reports_dir, prior_furl)
         save_to_png(fig, fname)
@@ -108,7 +108,7 @@ def render_marginal_report(param_name, param_range, param_prior, param_likelihoo
         plt.bar(np.array(param_range) - .5*param_step, param_likelihood, param_step)
         plt.xlabel(param_name)
         plt.ylabel('p(WTA|%s,M)' % param_name)
-        plt.ylim([0,.8])
+        plt.ylim([0,ylim])
         likelihood_furl = 'img/bayes_%s_marginal_likelihood_%s.png' % (file_prefix,param_name)
         fname = os.path.join(reports_dir, likelihood_furl)
         save_to_png(fig, fname)
@@ -120,7 +120,7 @@ def render_marginal_report(param_name, param_range, param_prior, param_likelihoo
         plt.bar(np.array(param_range) - .5*param_step, param_posterior, param_step)
         plt.xlabel(param_name)
         plt.ylabel('p(%s|WTA,M)' % param_name)
-        plt.ylim([0,.8])
+        plt.ylim([0,ylim])
         posterior_furl = 'img/bayes_%s_marginal_posterior_%s.png' % (file_prefix, param_name)
         fname = os.path.join(reports_dir, posterior_furl)
         save_to_png(fig, fname)
