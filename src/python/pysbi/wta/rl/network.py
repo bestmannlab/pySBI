504e11bf11bcbaf8c287cffb5062fdb62d057cda
jbonaiuto <jbonaiuto@gmail.com>
2014-03-17 12:59:04 +0000

    refactoring

--- src/python/pysbi/wta/rl/network.py
+++ src/python/pysbi/wta/rl/network.py
@@ -1,21 +1,22 @@
+import argparse
 from brian.clock import defaultclock
 from brian.stdunits import ms
 from brian.units import second
 import numpy as np
 import scipy.io
+import h5py
 from pysbi.wta.network import default_params, run_wta
+from pysbi.wta.rl.fit import fit_behavior
 
 
-def run_rl_simulation(mat_file):
+def run_rl_simulation(mat_file, wta_params, background_freq=5, output_file=None):
     mat = scipy.io.loadmat(mat_file)
-    prob_walk=mat['store']['dat'][0][0][0][0][5]
-    mags=mat['store']['dat'][0][0][0][0][7]
+    prob_walk=mat['store']['dat'][0][0][0][0][13]
+    mags=mat['store']['dat'][0][0][0][0][15]
 
-    wta_params=default_params
     num_groups=2
     exp_rew=np.array([0.5, 0.5])
     trial_duration=3*second
-    background_freq=5
     alpha=0.4
 
     trials=200
@@ -51,24 +52,24 @@ def run_rl_simulation(mat_file):
         choice[trial]=decision_idx+1
         rew[trial]=reward
 
-    probfile=open('prob.csv','w')
-    probfile.write(','.join([str(x) for x in prob_walk[0,0:trials]])+'\n')
-    probfile.write(','.join([str(x) for x in prob_walk[1,0:trials]]))
-    probfile.close()
-
-    magfile=open('mag.csv','w')
-    magfile.write(','.join([str(x) for x in mags[0,0:trials]])+'\n')
-    magfile.write(','.join([str(x) for x in mags[1,0:trials]]))
-    magfile.close()
-
-    outfile=open('choice.csv','w')
-    outfile.write(','.join([str(x) for x in choice]))
-    outfile.close()
-
-    rewfile=open('rew.csv','w')
-    rewfile.write(','.join([str(x) for x in rew]))
-    rewfile.close()
+    param_ests,prop_correct=fit_behavior(reward_probs, mags, rew, choice)
+    f = h5py.File(output_file, 'w')
+    f.attrs['alpha']=param_ests[0]
+    f.attrs['beta']=param_ests[1]
+    f.attrs['prop_correct']=prop_correct
+    f.close()
 
 
 if __name__=='__main__':
-    run_rl_simulation('../../data/rerw/subjects/1_2.mat')
+    ap = argparse.ArgumentParser(description='Run the WTA model')
+    ap.add_argument('--mat_file', type=str, default='../../data/rerw/subjects/value1_s1_t2.mat', help='Subject mat file')
+    ap.add_argument('--background', type=float, default=5.0, help='Background firing rate (Hz)')
+    ap.add_argument('--p_b_e', type=float, default=0.03, help='Connection prob from background to excitatory neurons')
+    ap.add_argument('--output_file', type=str, default=None, help='HDF5 output file')
+
+    argvals = ap.parse_args()
+
+    wta_params=default_params
+    wta_params.p_b_e=argvals.p_b_e
+
+    run_rl_simulation(argvals.mat_file, wta_params, background_freq=argvals.background, output_file=argvals.output_file)
--- src/python/pysbi/wta/run.py
+++ src/python/pysbi/wta/run.py
@@ -2,7 +2,7 @@ import os
 import random
 from brian.stdunits import nS, pA
 import numpy as np
-from brian.units import second, siemens, volt, amp
+from brian.units import second, siemens, amp
 from ezrcluster.launcher import Launcher
 from pysbi.wta.analysis import FileInfo, run_bayesian_analysis
 from pysbi.config import SRC_DIR
@@ -206,29 +206,6 @@ def post_one_param_wta_lesion_jobs(nodes, p_b_e, p_x_e, p_range, num_trials, sin
                     record_firing_rate=True, record_spikes=True, save_summary_only=True)
                 launcher.add_job(cmds, log_file_template=log_file_template, output_file=out_file)
 
-
-def post_wta_dcs_jobs(nodes, p_b_e, p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, background_freq, trials, start_nodes=True,
-                      p_dcs=0*pA, i_dcs=0*pA):
-    num_groups=2
-    trial_duration=4*second
-    input_sum=20.0
-    launcher=Launcher(nodes)
-    if start_nodes:
-        launcher.set_application_script(os.path.join(SRC_DIR, 'sh/ezrcluster-application-script.sh'))
-        launcher.start_nodes()
-
-    contrast_range=[0.0, 0.0625, 0.125, 0.25, 0.5, 1.0]
-    for i,contrast in enumerate(contrast_range):
-        inputs=np.zeros(2)
-        inputs[0]=(input_sum*(contrast+1.0)/2.0)
-        inputs[1]=input_sum-inputs[0]
-        for t in trials:
-            np.random.shuffle(inputs)
-            cmds,log_file_template,out_file=get_wta_cmds(num_groups, inputs, background_freq, trial_duration, p_b_e,
-                p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, contrast, t, p_dcs=p_dcs, i_dcs=i_dcs, record_lfp=True,
-                record_voxel=True, record_neuron_state=False, record_firing_rate=True, record_spikes=True,
-                save_summary_only=False)
-            launcher.add_job(cmds, log_file_template=log_file_template, output_file=out_file)
 
 def post_two_param_wta_jobs(nodes, p_b_e, p_x_e, p_range, num_trials, single_inh_pop=True, start_nodes=True,
                             muscimol_amount=0*nS, injection_site=0):
