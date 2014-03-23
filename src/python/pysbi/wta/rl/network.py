dont learn reward mag, fixed mag scaling, added p_x_e input arg

src/python/pysbi/wta/rl/network.py
@@ -13,6 +13,9 @@ def run_rl_simulation(mat_file, wta_params, background_freq=5, output_file=None)
     mat = scipy.io.loadmat(mat_file)
     prob_walk=mat['store']['dat'][0][0][0][0][13]
     mags=mat['store']['dat'][0][0][0][0][15]
+    prob_walk=prob_walk.astype(np.float32, copy=False)
+    mags=mags.astype(np.float32, copy=False)
+    mags /= 100.0
 
     num_groups=2
     exp_rew=np.array([0.5, 0.5])
@@ -24,30 +27,35 @@ def run_rl_simulation(mat_file, wta_params, background_freq=5, output_file=None)
     choice=np.zeros(trials)
     rew=np.zeros(trials)
     for trial in range(trials):
-        input_freq=np.array([exp_rew[0]-exp_rew[1], exp_rew[1]-exp_rew[0]])
+        ev=exp_rew*mags[:,trial]
+        input_freq=np.array([ev[0]-ev[1], ev[1]-ev[0]])
         input_freq=10.0+input_freq*2.5
-        reward_probs=prob_walk[:,trial]
-        reward_mags=mags[:,trial]/100.0
+
         trial_monitor=run_wta(wta_params, num_groups, input_freq, trial_duration, background_freq=background_freq,
             record_lfp=False, record_voxel=False, record_neuron_state=False, record_spikes=False,
             record_firing_rate=True, record_inputs=False, plot_output=False)
-        endIdx=int((trial_duration-1*second)/defaultclock.dt)
-        startIdx=endIdx-500
-        e_mean_final=[]
-        for i in range(num_groups):
-            rate_monitor=trial_monitor.monitors['excitatory_rate_%d' % i]
-            e_rate=rate_monitor.smooth_rate(width=5*ms, filter='gaussian')
-            e_mean_final.append(np.mean(e_rate[startIdx:endIdx]))
-        decision_idx=0
-        if e_mean_final[1]>e_mean_final[0]:
-            decision_idx=1
+
+        rate1_monitor=trial_monitor.monitors['excitatory_rate_0']
+        e_rate1=rate1_monitor.smooth_rate(width=5*ms, filter='gaussian')
+        rate2_monitor=trial_monitor.monitors['excitatory_rate_1']
+        e_rate2=rate2_monitor.smooth_rate(width=5*ms, filter='gaussian')
+        times=np.array(range(len(e_rate1)))*.0001
+        decision_idx=-1
+        for idx,time in enumerate(times):
+            if e_rate1[idx]>=30 and e_rate1[idx]>e_rate2[idx]:
+                decision_idx=0
+            elif e_rate2[idx]>=30 and e_rate2[idx]>e_rate1[idx]:
+                decision_idx=1
+
         print('Input frequencies=[%.2f, %.2f]' % (input_freq[0],input_freq[1]))
         print('Expected reward=[%.2f, %.2f]' % (exp_rew[0],exp_rew[1]))
         print('Decision=%d' % decision_idx)
+
         reward=0.0
-        if np.random.random()<=reward_probs[decision_idx]:
-            reward=reward_mags[decision_idx]
+        if np.random.random()<=prob_walk[decision_idx,trial]:
+            reward=1.0
         print('Reward=%.2f' % reward)
+
         exp_rew[decision_idx]=(1.0-alpha)*exp_rew[decision_idx]+alpha*reward
         choice[trial]=decision_idx
         rew[trial]=reward
@@ -66,6 +74,7 @@ if __name__=='__main__':
     ap = argparse.ArgumentParser(description='Run the WTA model')
     ap.add_argument('--mat_file', type=str, default='../../data/rerw/subjects/value1_s1_t2.mat', help='Subject mat file')
     ap.add_argument('--background', type=float, default=5.0, help='Background firing rate (Hz)')
+    ap.add_argument('--p_x_e', type=float, default=0.01, help='Connection prob from task inputs to excitatory neurons')
     ap.add_argument('--p_b_e', type=float, default=0.03, help='Connection prob from background to excitatory neurons')
     ap.add_argument('--output_file', type=str, default=None, help='HDF5 output file')
 
@@ -73,5 +82,6 @@ if __name__=='__main__':
 
     wta_params=default_params
     wta_params.p_b_e=argvals.p_b_e
+    wta_params.p_x_e=argvals.p_x_e
 
     run_rl_simulation(argvals.mat_file, wta_params, background_freq=argvals.background, output_file=argvals.output_file)