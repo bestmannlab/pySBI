fixed plot normalization

src/python/pysbi/wta/rl/analysis.py
@@ -378,7 +378,7 @@ def analyze_virtual_subjects(data_dir, num_virtual_subjects):
     fig=plt.figure()
     alpha_hist,alpha_bins=np.histogram(np.array(alpha_diff_vals), bins=10, range=(-1.0,1.0))
     bin_width=alpha_bins[1]-alpha_bins[0]
-    plt.bar(alpha_bins[:-1], alpha_hist/len(alpha_diff_vals), width=bin_width)
+    plt.bar(alpha_bins[:-1], alpha_hist/float(len(alpha_diff_vals)), width=bin_width)
     plt.xlim(-1.0,1.0)
     plt.xlabel('Change in alpha')
     plt.ylabel('Proportion of Subjects')
@@ -386,7 +386,7 @@ def analyze_virtual_subjects(data_dir, num_virtual_subjects):
     fig=plt.figure()
     beta_hist,beta_bins=np.histogram(np.array(beta_diff_vals), bins=10, range=(-10.0,10.0))
     bin_width=beta_bins[1]-beta_bins[0]
-    plt.bar(beta_bins[:-1], beta_hist/len(beta_diff_vals), width=bin_width)
+    plt.bar(beta_bins[:-1], beta_hist/float(len(beta_diff_vals)), width=bin_width)
     plt.xlim(-10.0,10.0)
     plt.xlabel('Change in beta')
     plt.ylabel('Proportion of Subjects')