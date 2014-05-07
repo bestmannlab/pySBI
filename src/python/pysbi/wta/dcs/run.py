9970b4f98b75cbcffa64e355bdaf5f23de942193
jbonaiuto <jbonaiuto@gmail.com>
2014-05-07 11:56:37 +0100

    added function to post broken dcs jobs

--- src/python/pysbi/wta/dcs/run.py
+++ src/python/pysbi/wta/dcs/run.py
@@ -6,6 +6,7 @@ from brian.units import second, siemens, amp
 from ezrcluster.launcher import Launcher
 from pysbi.config import SRC_DIR
 from pysbi.wta.run import get_wta_cmds
+from pysbi.wta.analysis import FileInfo
 
 def post_wta_dcs_jobs(nodes, p_b_e, p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, background_freq, trials, start_nodes=True,
                       p_dcs=0*pA, i_dcs=0*pA):
@@ -124,9 +125,31 @@ def run_wta_dcs_jobs(p_b_e, p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, background_freq, 
         inputs[1]=input_sum-inputs[0]
         for t in trials:
             np.random.shuffle(inputs)
-            cmds,log_file,out_file=get_wta_cmds(num_groups, inputs, background_freq, trial_duration, p_b_e,
+            cmds,log_file,out_file=get_dcs_cmds(num_groups, inputs, background_freq, trial_duration, p_b_e,
                 p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, contrast, t, p_dcs=p_dcs, i_dcs=i_dcs, record_lfp=True,
                 record_voxel=True, record_neuron_state=False, record_firing_rate=True, record_spikes=True,
                 save_summary_only=False)
             subprocess.Popen(cmds,stdout=log_file)
 
+
+def run_broken_dcs_jobs(p_b_e, p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, background_freq, trials, p_dcs=0*pA, i_dcs=0*pA):
+    num_groups=2
+    trial_duration=4*second
+    input_sum=20.0
+
+    contrast_range=[0.0, 0.0625, 0.125, 0.25, 0.5, 1.0]
+    for i,contrast in enumerate(contrast_range):
+        inputs=np.zeros(2)
+        inputs[0]=(input_sum*(contrast+1.0)/2.0)
+        inputs[1]=input_sum-inputs[0]
+        for t in trials:
+            np.random.shuffle(inputs)
+            cmds,log_file_name,out_file=get_dcs_cmds(num_groups, inputs, background_freq, trial_duration, p_b_e,
+                p_x_e, p_e_e, p_e_i, p_i_i, p_i_e, contrast, t, p_dcs=p_dcs, i_dcs=i_dcs, record_lfp=True,
+                record_voxel=True, record_neuron_state=False, record_firing_rate=True, record_spikes=True,
+                save_summary_only=False)
+            try:
+                data=FileInfo(out_file)
+            except:
+                log_file=open(log_file_name,'w  ')
+                subprocess.Popen(cmds,stdout=log_file)
