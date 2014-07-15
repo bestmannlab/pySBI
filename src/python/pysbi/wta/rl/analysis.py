8c86cd2fac40dae87cb17bb411360d8934209ead
jbonaiuto <jbonaiuto@gmail.com>
2014-07-15 17:19:41 +0100

    set xtick labels

--- src/python/pysbi/wta/rl/analysis.py
+++ src/python/pysbi/wta/rl/analysis.py
@@ -676,7 +676,8 @@ class RLReport:
                 std_diffs.append(np.std(self.stim_condition_rate_diffs[stim_condition])/np.sqrt(len(self.stim_condition_rate_diffs[stim_condition])))
             pos = np.arange(len(self.stim_conditions))+0.5    # Center bars on the Y-axis ticks
             ax.bar(pos,mean_diffs,width=.5,yerr=std_diffs,align='center',ecolor='k')
-            ax.set_xticks(pos, self.stim_conditions)
+            ax.set_xticks(pos)
+            ax.set_xticklabels(self.stim_conditions)
             save_to_png(fig, '%s.png' % fname)
             save_to_eps(fig, '%s.eps' % fname)
             plt.close(fig)
