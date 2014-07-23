fixed baseline rate computation

src/python/pysbi/wta/rl/analysis.py
@@ -516,10 +516,9 @@ class StimConditionReport:
                 session_report_file=os.path.join(self.data_dir,'%s.h5' % session_prefix)
                 data=FileInfo(session_report_file)
                 for trial in range(len(data.trial_e_rates)):
-                    pyr_rates.append((data.trial_e_rates[trial][0,500*ms/.5*ms:750*ms/.5*ms]+
-                                      data.trial_e_rates[trial][1,500*ms/.5*ms:750*ms/.5*ms])/2.0)
-                    inh_rates.append(data.trial_i_rates[trial][500*ms/.5*ms:750*ms/.5*ms])
-        print(pyr_rates)
+                    pyr_rates.append((data.trial_e_rates[trial][0,int((500*ms)/(.5*ms)):int((750*ms)/(.5*ms))]+
+                                      data.trial_e_rates[trial][1,int((500*ms)/(.5*ms)):int((750*ms)/(.5*ms))])/2.0)
+                    inh_rates.append(data.trial_i_rates[trial][int((500*ms)/(.5*ms)):int((750*ms)/(.5*ms))])
         return np.mean(pyr_rates),np.std(pyr_rates)/np.sqrt(len(pyr_rates)),np.mean(inh_rates),\
                np.std(inh_rates)/np.sqrt(len(inh_rates))
 