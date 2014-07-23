fixed baseline rate computation

src/python/pysbi/wta/rl/analysis.py
@@ -516,11 +516,21 @@ class StimConditionReport:
                 session_report_file=os.path.join(self.data_dir,'%s.h5' % session_prefix)
                 data=FileInfo(session_report_file)
                 for trial in range(len(data.trial_e_rates)):
-                    pyr_rates.append((data.trial_e_rates[trial][0,int((500*ms)/(.5*ms)):int((750*ms)/(.5*ms))]+
-                                      data.trial_e_rates[trial][1,int((500*ms)/(.5*ms)):int((750*ms)/(.5*ms))])/2.0)
-                    inh_rates.append(data.trial_i_rates[trial][int((500*ms)/(.5*ms)):int((750*ms)/(.5*ms))])
+                    pyr_rates.append(np.mean((data.trial_e_rates[trial][0,int((500*ms)/(.5*ms)):int((950*ms)/(.5*ms))]+
+                                      data.trial_e_rates[trial][1,int((500*ms)/(.5*ms)):int((950*ms)/(.5*ms))])/2.0))
+                    inh_rates.append(np.mean(data.trial_i_rates[trial][int((500*ms)/(.5*ms)):int((950*ms)/(.5*ms))]))
         return np.mean(pyr_rates),np.std(pyr_rates)/np.sqrt(len(pyr_rates)),np.mean(inh_rates),\
                np.std(inh_rates)/np.sqrt(len(inh_rates))
+
+#    def compute_ev_diff_rates(self):
+#        diff_rates=[]
+#        for virtual_subj_id in range(self.num_subjects):
+#            if virtual_subj_id not in self.excluded_sessions:
+#                session_prefix=self.file_prefix % (virtual_subj_id,self.stim_condition)
+#                session_report_file=os.path.join(self.data_dir,'%s.h5' % session_prefix)
+#                data=FileInfo(session_report_file)
+#                for trial in range(len(data.trial_e_rates)):
+#                    chosen_mean=
 
     def compute_trial_rate_stats(self, min_beta, max_beta, min_ev_diff, max_ev_diff):
         data=FileInfo(os.path.join(self.data_dir,'%s.h5' % self.file_prefix % (0,self.stim_condition)))