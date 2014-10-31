7b9aa3946cfaf8ee2aea1cc98dd3d8746b095088
jbonaiuto <jbonaiuto@gmail.com>
2014-10-31 15:14:43 +0000

    changed default run function

--- src/python/pysbi/wta/dcs/analysis.py
+++ src/python/pysbi/wta/dcs/analysis.py
@@ -593,8 +593,8 @@ class DCSComparisonReport:
 
 
 if __name__=='__main__':
-    dcs_report=DCSComparisonReport('/data/pySBI/rdmd/virtual_subjects/mu_40',
+    dcs_report=DCSComparisonReport('/data/pySBI/rdmd/virtual_subjects_half_dcs',
         'wta.groups.2.duration.4.000.p_e_e.0.080.p_e_i.0.100.p_i_i.0.100.p_i_e.0.200',range(20),
-        {'control':(0,0),'anode':(2,-1),'cathode':(-2,1)},25,
-        '/data/pySBI/reports/rdmd/postexp_sim_virtual_subjects_mu_40','')
-    dcs_report.create_report()
+        {'control':(0,0),'anode':(1,-0.5),'cathode':(-1,0.5)},25,
+        '/data/pySBI/reports/rdmd/postexp_sim_virtual_subjects_half_dcs','')
+    dcs_report.create_report(regenerate_subject_plots=False,regenerate_session_plots=False,regenerate_trial_plots=False)
