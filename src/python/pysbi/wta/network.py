36d583cd5f1bddd027e3cff775a0b5ddc5da2dd8
jeffinglis <jeffinglis@gmail.com>
2016-01-17 15:25:43 +0000

    added code for randomized trials over range of contrasts

--- src/python/pysbi/wta/network.py
+++ src/python/pysbi/wta/network.py
@@ -28,7 +28,7 @@ pyr_params=Parameters(
     gL=25*nS,
     refractory=2*ms,
     w_nmda = 0.165 * nS,
-    w_ampa_ext = 2.1*nS,
+    w_ampa_ext = 4 *nS,
     w_ampa_rec = 0.05*nS,
     w_gaba = 1.3*nS,
 )
@@ -208,7 +208,7 @@ class WTANetworkGroup(NeuronGroup):
                     delay=.5*ms)
                 self.connections['t%d->e%d_ampa' % (i,1-i)]=DelayConnection(self.task_inputs[i], self.groups_e[1-i],
                     'g_ampa_x')
-                self.connections['t%d->e%d_ampa' % (i,1-i)].connect_one_to_one(weight=.01 * self.pyr_params.w_ampa_ext,
+                self.connections['t%d->e%d_ampa' % (i,1-i)].connect_one_to_one(weight= .01 * self.pyr_params.w_ampa_ext,
                     delay=.5*ms)
 
 
